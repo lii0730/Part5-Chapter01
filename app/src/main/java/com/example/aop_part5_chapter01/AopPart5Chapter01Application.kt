b1695f838efccad7114fd8bc1cbbf1f099110cfa
leehsss <lii0730@icloud.com>
2021-07-01 22:16:28 +0900

    init setting

---
+++ app/src/main/java/com/example/aop_part5_chapter01/AopPart5Chapter01Application.kt
@@ -0,0 +1,21 @@
+package com.example.aop_part5_chapter01
+
+import android.app.Application
+import com.example.aop_part5_chapter01.DI.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class AopPart5Chapter01Application : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        //TODO Koin Trigger
+        startKoin {
+            androidLogger(Level.ERROR)
+            androidContext(this@AopPart5Chapter01Application)
+            modules(appModule)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/aop_part5_chapter01/DI/appModule.kt
@@ -0,0 +1,5 @@
+package com.example.aop_part5_chapter01.DI
+
+import org.koin.dsl.module
+
+internal val appModule = module {  }
